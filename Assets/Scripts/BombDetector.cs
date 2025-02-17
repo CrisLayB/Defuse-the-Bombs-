72b607ed89ae7079ef21fd58047e73cd545e6481
darielVilla01 <dariel.villatoro@outlook.com>
2025-02-17 15:10:20 -0600

    initial range detection

--- Assets/Scripts/BombDetector.cs
+++ Assets/Scripts/BombDetector.cs
@@ -4,36 +4,39 @@ using UnityEngine;
 
 public class BombDetector : MonoBehaviour
 {
-    [SerializeField] private AudioSource _detectorSound;
+    [SerializeField] private AudioClip _detectorSound;
+    private AudioSource _detectorSource;
+    private float nextPlayTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _detectorSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerStay(Collider other)
     {
+
         if (other.CompareTag("Bomb")) {
             // Get distance
             Vector3 bombPosition = other.gameObject.transform.position;
             Vector3 playerPosition = gameObject.transform.position;
 
-            float distance = Mathf.Sqrt(
-                Mathf.Pow(bombPosition.x - playerPosition.x, 2f) +
-                Mathf.Pow(bombPosition.y - playerPosition.y, 2f) +
-                Mathf.Pow(bombPosition.z - playerPosition.z, 2f));
+            float distance = Vector3.Distance(bombPosition, playerPosition);
 
             // Debug.Log(distance);
             
             if (distance > 8f) {
-                new WaitForSeconds(1f);
-                Debug.Log("Play Sound");
+                if (Time.time >= nextPlayTime) {
+                    Debug.Log("Play Sound");
+                    _detectorSource.PlayOneShot(_detectorSound);
+                    nextPlayTime += 0.25f;
+                }
             }
         }
     }
