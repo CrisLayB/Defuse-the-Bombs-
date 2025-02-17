0745e4bee6944d98e6f6245b8ce967eb458d951e
darielVilla01 <dariel.villatoro@outlook.com>
2025-02-17 17:15:22 -0600

    functional bomb detection by proximity

--- Assets/Scripts/BombDetector.cs
+++ Assets/Scripts/BombDetector.cs
@@ -21,21 +21,34 @@ public class BombDetector : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-
-        if (other.CompareTag("Bomb")) {
+        if (other.CompareTag("Player")) {
+            float colliderRadius = GetComponent<SphereCollider>().radius;
+            float realRadius = colliderRadius * transform.localScale.x; //Asumes restricted scaling
+            
             // Get distance
-            Vector3 bombPosition = other.gameObject.transform.position;
-            Vector3 playerPosition = gameObject.transform.position;
+            Vector3 bombPosition = gameObject.transform.position;
+            Vector3 playerPosition = other.gameObject.transform.position;
 
             float distance = Vector3.Distance(bombPosition, playerPosition);
-
-            // Debug.Log(distance);
             
-            if (distance > 8f) {
+            // A better implementation should be possible, but this is fine for now.
+            if (distance > (2*realRadius / 3)) {
                 if (Time.time >= nextPlayTime) {
                     Debug.Log("Play Sound");
                     _detectorSource.PlayOneShot(_detectorSound);
-                    nextPlayTime += 0.25f;
+                    nextPlayTime += 1f;
+                }
+            } else if (distance <= (2*realRadius / 3) && distance >= (realRadius / 3)) {
+                if (Time.time >= nextPlayTime) {
+                    Debug.Log("Play Sound");
+                    _detectorSource.PlayOneShot(_detectorSound);
+                    nextPlayTime += 0.6f;
+                }
+            } else {
+                if (Time.time >= nextPlayTime) {
+                    Debug.Log("Play Sound");
+                    _detectorSource.PlayOneShot(_detectorSound);
+                    nextPlayTime += 0.3f;
                 }
             }
         }
