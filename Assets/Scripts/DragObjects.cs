a4b94f54d23fa931371877ca7a47e1c8d2d61021
darielVilla01 <dariel.villatoro@outlook.com>
2025-02-18 00:40:23 -0600

    functional drag and drop for draggable objects

--- Assets/Scripts/DragObjects.cs
+++ Assets/Scripts/DragObjects.cs
@@ -1,17 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class DragObjects : MonoBehaviour
 {
+    [SerializeField] private LayerMask _draggableMask;
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private float _moveSpeed = 10f;
+    private Rigidbody _selectedObject;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = new Ray(_cameraTransform.position, _cameraTransform.forward);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 5.0f))
+            if (Physics.Raycast(ray, out hit, 4.5f, _draggableMask))
             {
-                Debug.Log("Hit");
+                _selectedObject = hit.rigidbody;
+                _selectedObject.useGravity = false;
+            }
+        }
+
+        if (_selectedObject) {
+            Vector3 targetPosition = _cameraTransform.position + _cameraTransform.forward * 2.5f;
+            Vector3 direction = targetPosition - _selectedObject.position;
+            _selectedObject.velocity = direction * _moveSpeed;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            if (_selectedObject) {
+                _selectedObject.useGravity = true;
+                // _selectedObject.velocity = Vector3.zero;
+                _selectedObject = null;
             }
         }
     }
