4ddbc35f98511c655d85c331daba47461b53ad62
darielVilla01 <dariel.villatoro@outlook.com>
2025-02-19 18:57:39 -0600

    functional mechanography UI

--- Assets/Scripts/MiniGames/Mechanography.cs
+++ Assets/Scripts/MiniGames/Mechanography.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
 
 public class Mechanography : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _sentenceTextBox;
+
+    [Header("UnityEvents")]
+    [SerializeField] private UnityEvent _winEvent;
+    [SerializeField] private UnityEvent _loseEvent;
+
     string[] _sentences = {"good morning", "nice weather", "i didn't do anything at all"};
+    string _correctSentence;
     int _sentenceIndex = 0;
     int _characterIndex = 0;
 
     void Start()
     {
-        
+        _sentenceTextBox.text = _sentences[_sentenceIndex];
     }
 
     void Update()
@@ -17,15 +26,21 @@ public class Mechanography : MonoBehaviour
             char input = Input.inputString[0];
             if (_sentences[_sentenceIndex][_characterIndex].Equals(input))
             {
-                Debug.Log("Key: " + input);
-                _characterIndex += 1;
+                _correctSentence += input;
+                string text = _sentences[_sentenceIndex];
+                text = text.Remove(0,_correctSentence.Length).Insert(0, "<color=green>"+_correctSentence+"</color>");
+                _sentenceTextBox.text = text;
+
+                _characterIndex++;
                 if (_characterIndex >= _sentences[_sentenceIndex].Length)
                 {
-                    _sentenceIndex += 1;
+                    _sentenceIndex++;
                     _characterIndex = 0;
+                    _correctSentence = "";
+                    _sentenceTextBox.text = _sentences[_sentenceIndex];
                 }
             } else {
-                Debug.Log("Failed");
+                _sentenceTextBox.text = "<color=red>" + _sentenceTextBox.text + "</color>";
             }
         }
     }
